9442307d1a788fe6a687d70a8dd088616c7452fe
T.Alexa <alejandra.marianella.8@gmail.com>
2023-03-18 08:52:22 +0100

    Dolgozat

---
+++ objektumok.js
@@ -0,0 +1,20 @@
+window.addEventListener("load",init())
+
+function init(){
+    ElsoFeladat();
+
+}
+
+function ElsoFeladat(){
+    const kutyaLista=["foxterrier","kuvasz","puli","agár","újfullandi","keverék"];
+    ELEM = addEventListener("click", function(){gomb()})
+    text = "";
+    for (var i = 0; i < kutyaLista.length; i++){
+        text += 
+    }
+}
+
+function gomb(){
+        document.getElementById("kutya").innerHTML = "Kutya";
+}
+
