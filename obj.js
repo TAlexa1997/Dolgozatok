1370b832aa3611f48cc4e68d989fce3c9251451c
T.Alexa <alejandra.marianella.8@gmail.com>
2023-03-18 10:34:09 +0100

    css formázások

--- obj.js
+++ obj.js
@@ -1,27 +1,28 @@
 window.addEventListener("load", init);
 const KUTYAK =[
-
+    {
+        nev: "Teo",
+        fajta: "Bischon Havanese",
+        láb: 4,
+        marmagassága: 52,
+        nem: "kan",
+        kor: 5,
+      },
+      {
+        nev: "Bizsu",
+        fajta: "Bischon Havanese",
+        lab: 4,
+        marmagassága: 20,
+        nem: "szuka",
+        kor: "4 hónapos",
+    }
 ]
+
+
 function init() {
     // objektum
     //kulcs-értékpárok
-  const Teo = {
-    nev: "Teo",
-    fajta: "Bischon Havanese",
-    láb: 4,
-    marmagassága: 52,
-    nem: "kan",
-    kor: 5,
-  };
-  const Bizsu = {
-    nev: "Teo",
-    fajta: "Bischon Havanese",
-    lab: 4,
-    marmagassága: 20,
-    nem: "szuka",
-    kor: "4 hónapos",
-  };
-  console.log(Teo);
+  /**console.log(Teo);
   console.log(Bizsu);
   console.log(Teo.nev);
   console.log(Bizsu.nev);
@@ -31,8 +32,16 @@ function init() {
   console.log(Bizsu);
   delete Bizsu.lab;
   console.log(Bizsu);
-  /** Az összes kulcs(objektumok) bejárása forin ciklussal */
+  Az összes kulcs(objektumok) bejárása forin ciklussal 
   for (const kulcs in Teo) {
    console.log(kulcs,Teo[kulcs]);
+  }*/
+  for (let index = 0; index < kutyaLista.length; index++) {
+    for (const kulcs in kutyaLista[index]) {
+        console.log(kulcs,kutyaLista[index][kulcs]);
+      }
   }
+  
+  console.log();
+
 }
--- objektumok.js
+++ objektumok.js
@@ -1,12 +1,39 @@
 window.addEventListener("load", init);
 const kutyaLista = [
-    "foxterrier",
-    "kuvasz",
-    "puli",
-    "agár",
-    "újfullandi",
-    "keverék",
-  ];
+  {
+    nev: "Teo",
+    fajta: "Bischon Havanese",
+    láb: 4,
+    marmagassága: 52,
+    nem: "kan",
+    kor: 5,
+  },
+  {
+    nev: "Bizsu",
+    fajta: "Bischon Havanese",
+    lab: 4,
+    marmagassága: 20,
+    nem: "szuka",
+    kor: "4 hónapos",
+  },
+  {
+    nev: "Pamacs",
+    fajta: "Shetlandi Juhászkutya",
+    lab: 4,
+    marmagassága: 60,
+    nem: "szuka",
+    kor: 13,
+  },
+  {
+    nev: "Louis",
+    fajta: "Keverék",
+    lab: 4,
+    marmagassága: 70,
+    nem: "kan",
+    kor: 11,
+  },
+];
+
 /** function init() {
   const kutyaLista = [
     "foxterrier",
@@ -38,14 +65,42 @@ function init() {
 
 function osszeallit() {
   // összállítjuk azt a szöveges tartalmat, amia HTML kódo jelenti
-  let txt="";
-  for (let index =0; index < kutyaLista.length; index++){
-    txt += `<div>
-                <p>
-                    ${kutyaLista[index]}
-                </p>
-            </div>`
+  // console.log(kulcs,kutyaLista[index][kulcs]);
+  let txt = "";
+  for (let index = 0; index < kutyaLista.length; index++) {
+    for (const kulcs in kutyaLista[index]) {
+      txt += `<div>
+                    <p>
+                        ${(kulcs,kutyaLista[index][kulcs[0]])}
+                    </p>
+                    <p>
+                        ${(kulcs, kutyaLista[index][kulcs[1]])}
+                    </p>
+                    <p>
+                        ${(kulcs, kutyaLista[index][kulcs[2]])}
+                    </p>
+                    <p>
+                        ${(kulcs, kutyaLista[index][kulcs[3]])}
+                    </p>
+                    <p>
+                        ${(kulcs, kutyaLista[index][kulcs[4]])}
+                    </p>
+                    <p>
+                        ${(kulcs, kutyaLista[index][kulcs[5]])}
+                    </p>
+                </div>`;
+    }
   }
+  for (let index = 0; index < kutyaLista.length; index++) {
+    for (const kulcs in kutyaLista[index]) {
+        console.log(kulcs,kutyaLista[index][kulcs]);
+      }
+  }
+  
+  console.log();
   console.log(txt);
-  return txt
+  return txt;
+
+  
 }
+
